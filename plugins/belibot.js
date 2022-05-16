105f65adcca1a7d40044048f2a054dacd7e415d6
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-16 15:14:14 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -7,16 +7,16 @@ let handler = async (m, { conn }) => {
 ╠➥ *Permanen* : *Rp 40000*
 ╠➥ *Owner* :   *Rp 30000*/bln
 ╠➥ *Nomer Gopay & pulsa* : 
-║    085730903853
+║    > Ke Owner <
 ╠═〘 PEMBAYARAN 〙 ═
 ╠➥ Dana Dan Pulsa
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 AlyaaXzy 〙 ═`.trim(), watermark, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
+╠═ ©1992 Rpg Rull-MD
+╠═ Script original by Nurutomo
+╠═〘 Rull 〙 ═`.trim(), watermark, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
 }
 
 handler.command = /^sewabot$/i
